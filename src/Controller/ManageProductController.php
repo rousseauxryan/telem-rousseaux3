057e359580f236a317f8c9ac28050f1d5affe477
DrakiorTV <fandedrakior@gmail.com>
2025-03-19 16:07:20 +0100

    suppression fonctionnelle

--- src/Controller/ManageProductController.php
+++ src/Controller/ManageProductController.php
@@ -55,7 +55,7 @@ class ManageProductController extends AbstractController
         );
     }
 
-    #[Route('manage/product/edit/{id}', name: 'manage_product_edit')]
+    #[Route('manage/product/edit/{id}', name: 'manage_product_edit', requirements: ['id'=>'\d+'])]
     public function edit(Product $product, Request $request, EntityManagerInterface $em):Response
     {
 
@@ -95,7 +95,34 @@ class ManageProductController extends AbstractController
 
         return $this->renderForm('product/product_new.html.twig', [
             'form' => $form,
+            'product'=>$product,
         ]);
 
     }
+
+    #[Route('manage/product/delete/{id}', name: 'manage_product_delete', requirements: ['id'=>'\d+'])]
+    public function delete(Product $product, Request $request, EntityManagerInterface $em):Response{
+
+        $id = $product->getId();
+        $em->remove($product);
+        $em->flush();
+
+        $this->addFlash('success', 'Le produit '.$id.' a été supprimé.');
+
+        return $this->redirectToRoute('product_show_all');
+
+    }
+
+
+    #[Route('manage/product/delete-confirm/{id}', name: 'manage_product_delete_confirm', requirements: ['id'=>'\d+'])]
+    public function deleteConfirm(Product $product){
+
+        return $this->render('product/product_delete_confirm.html.twig', [
+            'product'=>$product,
+        ]);
+
+    }
+
+
+
 }
